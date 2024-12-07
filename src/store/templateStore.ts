ccbd5929eadd04fe42bedff4793bf303d8371891
ChanLee_KR <cl700120@gmail.com>
2024-12-07 23:04:23 +0900

    feat: 템플릿 스토어 추가

---
+++ src/store/templateStore.ts
@@ -0,0 +1,94 @@
+'use client'
+
+import React from 'react'
+import { create } from 'zustand'
+import type { Template, Content } from '@/types'
+import { TEMPLATES } from '@/data'
+
+interface TemplateStore {
+  currentId: Template['id']
+  backgroundURL: Template['backgroundURL']
+  ratio: Template['ratio']
+
+  contents: Template['contents']
+  contentsLength: number
+  activeContentIndex: number
+
+  initTemplate: (template: Template) => void
+
+  focusContent: (index: number) => void
+
+  modifyContent: (payload: Partial<Content>) => void
+}
+
+const useTemplateStore = create<TemplateStore>((set) => ({
+  currentId: TEMPLATES[0].id,
+  backgroundURL: TEMPLATES[0].backgroundURL,
+  ratio: TEMPLATES[0].ratio,
+
+  contents: TEMPLATES[0].contents,
+  contentsLength: TEMPLATES[0].contents.length,
+  activeContentIndex: 0,
+
+  initTemplate: (template) =>
+    set((prev) => {
+      if (template.id === prev.currentId) {
+        return prev
+      }
+
+      return {
+        currentId: template.id,
+        ratio: template.ratio,
+        backgroundURL: template.backgroundURL,
+        contents: template.contents,
+        contentsLength: template.contents.length,
+        activeContentIndex: 0,
+      }
+    }),
+
+  focusContent: (index) => set({ activeContentIndex: index }),
+
+  modifyContent: (payload) =>
+    set(({ contents, activeContentIndex }) => {
+      return {
+        contents: contents.map((content, idx) => {
+          if (idx === activeContentIndex) {
+            return { ...content, ...payload }
+          }
+
+          return content
+        }),
+      }
+    }),
+}))
+
+export const useActiveContentIndex = () => useTemplateStore((store) => store.activeContentIndex)
+
+export const useTemplate = () => {
+  const currentId = useTemplateStore((store) => store.currentId)
+  const ratio = useTemplateStore((store) => store.ratio)
+  const backgroundURL = useTemplateStore((store) => store.backgroundURL)
+  const contentsLength = useTemplateStore((store) => store.contentsLength)
+
+  return React.useMemo(() => {
+    return { currentId, backgroundURL, ratio, contentsLength }
+  }, [currentId, backgroundURL, ratio, contentsLength])
+}
+
+export const useTemplateContent = (index: number) => {
+  return useTemplateStore(({ contents }) => contents[index])
+}
+
+export const useCurrentContent: <K extends keyof TemplateStore['contents'][number]>(
+  key: K,
+) => TemplateStore['contents'][number][K] = (key) => {
+  const activeContentIndex = useActiveContentIndex()
+
+  return useTemplateStore((store) => store.contents[activeContentIndex][key])
+}
+
+export const useInitTemplate = () => useTemplateStore((store) => store.initTemplate)
+
+export const useFocusContent = () => useTemplateStore((store) => store.focusContent)
+
+export const useModifyContent = () => useTemplateStore((store) => store.modifyContent)
