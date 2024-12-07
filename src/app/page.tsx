b17a65ce0919fbf2b672de72ef83b8d9c80339ed
ChanLee_KR <cl700120@gmail.com>
2024-12-07 23:10:24 +0900

    design: 메인 페이지 반응형 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ const Main = () => {
 
   return (
     <div className="relative flex min-h-dvh flex-col items-center justify-end">
-      <div className="absolute top-0 flex h-[calc(100dvh-20rem)] w-full flex-col items-center justify-start bg-[url(/img/main-cover.png)] bg-cover bg-bottom bg-no-repeat pb-12 pt-36">
-        <div className="mx-auto mb-16 flex size-28 items-center justify-center rounded-3xl border-4 border-gray-700 bg-white">
+      <div className="absolute top-0 flex h-[calc(100dvh-20rem)] w-full flex-col items-center justify-start bg-[url(/img/main-cover.png)] bg-cover bg-bottom bg-no-repeat pb-12 pt-[12vh]">
+        <div className="mx-auto mb-[7vh] flex size-28 items-center justify-center rounded-3xl border-4 border-gray-700 bg-white">
           <Image
             src="/img/main-icon.png"
             alt="메인 아이콘"
