9ecd2fb84f3c397eea356349abe0dea03f57f580
ChanLee_KR <cl700120@gmail.com>
2024-12-08 17:40:09 +0900

    refactor: 조회수 컴포넌트 에러 처리 추가

--- src/components/ViewCount.tsx
+++ src/components/ViewCount.tsx
@@ -2,13 +2,25 @@ import { formatNumber } from '@/utils'
 import { getCount } from '@/utils'
 
 const ViewCount = async () => {
-  const count = await getCount()
+  try {
+    const count = await getCount()
 
-  return (
-    <p className="mt-12 break-keep text-center text-lg font-medium">
-      {formatNumber(count)}번째 부적을 공유해주세요!
-    </p>
-  )
+    return (
+      <p className="mt-12 break-keep text-center text-lg font-medium">
+        {formatNumber(count)}번째 부적을 공유해주세요!
+      </p>
+    )
+  } catch {
+    return (
+      <p className="mt-12 flex flex-wrap items-center justify-center break-keep text-center text-lg font-medium">
+        <span className="mr-1 inline-flex items-center justify-center whitespace-nowrap">
+          <span className="mr-1 h-5 w-16 animate-pulse rounded bg-gray-200" />
+          번째{' '}
+        </span>
+        부적을 공유해주세요!
+      </p>
+    )
+  }
 }
 
 export default ViewCount
