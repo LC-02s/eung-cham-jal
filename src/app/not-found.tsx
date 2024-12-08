d3a47af1faebdd1ae5fc58cca17528ccac3c1ade
ChanLee_KR <cl700120@gmail.com>
2024-12-08 11:33:34 +0900

    fix: not found page 서버 컴포넌트로 변경

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,10 @@
-'use client'
-
-import { useRouter } from 'next/navigation'
 import Image from 'next/image'
 import Link from 'next/link'
 import { EFJejudoldam } from './fonts'
-import { Button } from '@/components'
+import { BackButton, Button } from '@/components'
 import { cn } from '@/lib/utils'
 
 const NotFound = () => {
-  const { back } = useRouter()
-
   return (
     <div className="relative flex min-h-dvh w-full flex-col items-center justify-end">
       <div className="flex w-full flex-1 flex-col items-center justify-center px-6 pb-12 pt-20">
@@ -30,9 +25,7 @@ const NotFound = () => {
             메인으로 돌아가기
           </Link>
         </Button>
-        <Button variant="custom" title="이전으로 돌아가기" className="mt-6" onClick={back}>
-          이전으로 돌아가기
-        </Button>
+        <BackButton />
       </div>
     </div>
   )
---
+++ src/components/BackButton.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { useRouter } from 'next/router'
+import React from 'react'
+import { Button } from './ui'
+
+const BackButton = () => {
+  const { back } = useRouter()
+
+  return (
+    <Button variant="custom" title="이전으로 돌아가기" className="mt-6" onClick={back}>
+      이전으로 돌아가기
+    </Button>
+  )
+}
+
+export default BackButton
