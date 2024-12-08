cfd067432a205000cd27023488521a4f916d5004
ChanLee_KR <cl700120@gmail.com>
2024-12-08 19:19:51 +0900

    design: 셀렉트 드롭다운 최대 높이 조정

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -69,7 +69,7 @@ const SelectContent = React.forwardRef<
     <SelectPrimitive.Content
       ref={ref}
       className={cn(
-        'relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-2xl border-2 border-gray-700 bg-popover text-popover-foreground shadow-[0px_2px_0px_1px_#40403F] data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-4 data-[side=left]:slide-in-from-right-4 data-[side=right]:slide-in-from-left-4 data-[side=top]:slide-in-from-bottom-4',
+        'relative z-50 max-h-[42vh] min-w-[8rem] overflow-hidden rounded-2xl border-2 border-gray-700 bg-popover text-popover-foreground shadow-[0px_2px_0px_1px_#40403F] data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-4 data-[side=left]:slide-in-from-right-4 data-[side=right]:slide-in-from-left-4 data-[side=top]:slide-in-from-bottom-4',
         position === 'popper' &&
           'data-[side=bottom]:translate-y-2 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-2',
         className,
