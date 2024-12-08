a6a5bf3aca8d3421a3912ce5d4998d35850088d8
ChanLee_KR <cl700120@gmail.com>
2024-12-08 19:45:19 +0900

    fix: 조회수 갱신 기능 수정

--- src/app/api/notion/route.ts
+++ src/app/api/notion/route.ts
@@ -1,7 +1,22 @@
-import { increaseCount } from '@/utils'
+import { getCount, increaseCount } from '@/utils'
 import { NextResponse } from 'next/server'
 
+export async function GET() {
+  try {
+    const count = await getCount()
+
+    return NextResponse.json({ count })
+  } catch {
+    return NextResponse.json({ count: -1 }, { status: 500 })
+  }
+}
+
 export async function POST() {
-  await increaseCount()
-  return NextResponse.json({ message: 'success' })
+  try {
+    await increaseCount()
+
+    return NextResponse.json({ message: 'success' })
+  } catch {
+    return NextResponse.json({ message: 'failed' }, { status: 500 })
+  }
 }
--- src/components/ViewCount.tsx
+++ src/components/ViewCount.tsx
@@ -1,16 +1,19 @@
+'use client'
+
+import React from 'react'
 import { formatNumber } from '@/utils'
-import { getCount } from '@/utils'
 
-const ViewCount = async () => {
-  try {
-    const count = await getCount()
+const ViewCount = () => {
+  const [count, setCount] = React.useState(-1)
 
-    return (
-      <p className="mt-12 break-keep text-center text-base font-medium md:text-lg">
-        {formatNumber(count)}번째 부적을 만들어 주세요!
-      </p>
-    )
-  } catch {
+  React.useEffect(() => {
+    fetch('/api/notion')
+      .then((data) => data.json())
+      .then(({ count }) => setCount(count))
+      .catch(() => setCount(-1))
+  }, [])
+
+  if (count < 0) {
     return (
       <p className="mt-12 flex flex-wrap items-center justify-center break-keep text-center text-base font-medium md:text-lg">
         <span className="mr-1 inline-flex items-center justify-center whitespace-nowrap">
@@ -21,6 +24,12 @@ const ViewCount = async () => {
       </p>
     )
   }
+
+  return (
+    <p className="mt-12 break-keep text-center text-base font-medium md:text-lg">
+      {formatNumber(count)}번째 부적을 만들어 주세요!
+    </p>
+  )
 }
 
 export default ViewCount
