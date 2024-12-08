1743bc73928928f0293a3ce8140c78dc9da69f28
ChanLee_KR <cl700120@gmail.com>
2024-12-08 11:26:00 +0900

    fix: 메타 데이터 내 metadataBase 값 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import { Toaster } from '@/components/ui/sonner'
 
 export const metadata: Metadata = {
+  metadataBase: new URL(process.env.NEXT_PUBLIC_DOMAIN || ''),
   title: '응원 참 잘하는 집 - 나만의 부적 메이커',
   description: '응원 참 잘하는 집과 함께 나만의 부적을 만들어 보세요!',
   openGraph: {
